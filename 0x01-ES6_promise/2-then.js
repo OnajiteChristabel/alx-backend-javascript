a5711e8a2dc7ef5a117bcccb37cf77767bc94ece
OnajiteChristabelO0 <christabelonajite2005@gmail.com>
2023-06-30 04:16:17 +0000

    y

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,3 +3,4 @@ export default function handleResponseFromAPI(promise) {
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => Error())
     .finally(() => { console.log('Got a response from the API'); });
+}

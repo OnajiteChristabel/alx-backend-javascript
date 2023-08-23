cec93770a16b5a0e9e01a099aa463eb87727ec4d
OnajiteChristabel <111015651+OnajiteChristabel@users.noreply.github.com>
2023-08-23 02:10:39 +0100

    Update 6-http_express.js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,13 +1,13 @@
 const express = require('express');
+
 const app = express();
 const port = 1245;
 
 app.get('/', (request, response) => {
-  response.send('Hello Holberton School!\n');
+  response.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running and listening on port ${port}`);
 });
 
 module.exports = app;
